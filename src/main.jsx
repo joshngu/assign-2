c9d711fd4a72275db3f6442d9d0d107d05b8df20
HaffyAK <hafsaazamkhan@gmail.com>
2026-07-24 07:20:29 -0500

    Remove NotificationProvider from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import QueueSmartAuth from "./QueueSmartAuth";
-import { NotificationProvider } from "./Notifications";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NotificationProvider>
-      <QueueSmartAuth />
-    </NotificationProvider>
+    <QueueSmartAuth />
   </React.StrictMode>
 );
